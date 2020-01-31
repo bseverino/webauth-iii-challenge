44cfc8988f54ae10955c8ef7afe444f9e4e3a2c9
B Severino <biancasev@gmail.com>
2020-01-30 20:13:11 -0500

    Changed console.log in index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = require('./api/server.js')
 const port = process.env.PORT
 
 server.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`)
+    console.log(`Server is listening on port ${port}!!!`)
 })
